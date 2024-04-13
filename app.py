ee61eb3f4dc164e16b84ecd76e4858817134f253
Aayush suagndhi <aayush.sugandhi@gmail.com>
2024-04-13 21:47:56 +0530

    rendered index with flask

--- app.py
+++ app.py
@@ -1,14 +1,16 @@
-from flask import Flask
+from flask import Flask, render_template # Import render_template function to render the template of template folder     
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def hello_world():
-    return "<p>Hello, World!</p>"
+    return render_template('index.html')
 
 @app.route("/about")
 def about():
     return "<p>About page</p>"
+
 
 if __name__ == "__main__":
     app.run(debug=True, port=8000)
