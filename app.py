683a6fbaa3d408a7276fe0480b07311e8903c999
Aayush suagndhi <aayush.sugandhi@gmail.com>
2024-04-13 23:46:51 +0530

    created application to show name time and CWD

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template # Import render_template function to render the template of template folder     
+from op import Flask, render_template # Import render_template function to render the template of template folder     
 
 app = Flask(__name__)
 
---
+++ op.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template
+import datetime
+import os
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    name = "Aayush"  # Replace with your name
+    current_time = datetime.datetime.now().strftime("%I:%M:%S")
+    current_directory = os.getcwd()
+    return render_template('index.html', name=name, current_time=current_time, current_directory=current_directory)
+
+if __name__ == '__main__':
+    app.run(debug=True, port=8000)
