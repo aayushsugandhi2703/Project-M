7fae71134222b545a3d64c0262fbbf1683e4fea7
Aayush suagndhi <aayush.sugandhi@gmail.com>
2024-04-13 21:31:12 +0530

    added subdomain page

--- app.py
+++ app.py
@@ -6,6 +6,9 @@ app = Flask(__name__)
 def hello_world():
     return "<p>Hello, World!</p>"
 
+@app.route("/about")
+def about():
+    return "<p>About page</p>"
 
 if __name__ == "__main__":
     app.run(debug=True, port=8000)
