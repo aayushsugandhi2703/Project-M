fc4e9e97fc34aa60aebe0591f63cea05dbe829f5
Aayush suagndhi <146061741+aayushsugandhi2703@users.noreply.github.com>
2024-05-24 10:40:56 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,16 +1,30 @@
-from op import Flask, render_template # Import render_template function to render the template of template folder     
+from flask import Flask, render_template,jsonify
+import datetime
+import os
 
 app = Flask(__name__)
 
+@app.route('/')
+def index():
+    name = "Aayush"  # Replace with your name
+    current_time = datetime.datetime.now().strftime("%I:%M:%S")
+    current_directory = os.getcwd()
+    return render_template('index.html', name=name, current_time=current_time, current_directory=current_directory)
 
-@app.route("/")
-def hello_world():
-    return render_template('index.html')
+@app.route("/content/<int:age>")
+def content(age):
+    if age < 18:
+        result = {
+            "content": "You are not allowed to access this content",
+            "message": "You are under 18",
+            "error": 404
+        }
+    else:
+        result = {
+            "content": "You are allowed to access this content",
+            "message": "Welcome to the content"
+        }
+    return jsonify(result)
 
-@app.route("/about")
-def about():
-    return "<p>About page</p>"
-
-
-if __name__ == "__main__":
-    app.run(debug=True, port=8000)
+if __name__ == '__main__':
+    app.run(debug=False)
